4b78f7344148cd75f2d981fadb62bd65c65201e1
kristy1992 <61727308+kristy1992@users.noreply.github.com>
2020-08-08 15:26:34 +0530

    demowebapplication

---
+++ DemoWebApplication/DemoWebApplication/Controller/SampleController.cs
@@ -0,0 +1,30 @@
+﻿namespace DemoWebApplication.Controller
+{
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        #region Private Fields
+
+        private const string DDIM_FilePath = "ddim.json";
+
+        #endregion    
+
+        #region Actions
+
+        /// <summary>
+        /// Returns DDIM i.e. metadata of all features.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("default")]
+        public ActionResult<string> GetDefaultDesignIntentModel()
+        {
+            return System.IO.File.ReadAllText(DDIM_FilePath);
+        }
+
+        #endregion
+    }
+}
